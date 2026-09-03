172ac69a0ed6b480157941a7eb36b56bf17e173f
mansoor-AS14723 <mansoordevsoft@gmail.com>
2026-09-03 11:27:47 +0500

    TypeScript learning practice1 done

---
+++ index.js
@@ -0,0 +1,7 @@
+let message = "you are welcome to typescript";
+console.log(message);
+let name = "Mansoor";
+console.log(name);
+let age = 28;
+console.log(`${name}  ${message} your's age is ${age} am i right.`);
+export {};
---
+++ index.ts
@@ -0,0 +1,6 @@
+let message : string = "you are welcome to TypeScript."
+console.log(message)
+let username : string = "Mansoor"
+console.log(username)
+let age:number = 28
+console.log(`Hello ${username}  ${message} your's age is ${age} am i right.`)
